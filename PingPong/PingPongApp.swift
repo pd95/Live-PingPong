25f8e769f061290528326b8fc38ed9322b7bc386
Philipp <pd95@users.noreply.github.com>
2022-07-04 19:56:52 +0200

    Initial Commit

---
+++ PingPong/PingPongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPongApp.swift
+//  PingPong
+//
+//  Created by Philipp on 04.07.22.
+//
+
+import SwiftUI
+
+@main
+struct PingPongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
